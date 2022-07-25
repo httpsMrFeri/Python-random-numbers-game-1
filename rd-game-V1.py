0c7e5863f94d25e127853d2ca21af0df2c0eb244
Mr Feri <109946641+o0mrferi0o@users.noreply.github.com>
2022-07-25 12:48:42 +0430

    Create rd-game-V1.py
    pip install random

---
+++ rd-game-V1.py
@@ -0,0 +1,13 @@
+import random
+num = (random.randrange(1, 10))
+num = int(num)
+while (1) :
+    num1=input("enter random number : ")
+    num1=int(num1)
+    if num1>num : 
+        print ("\n** number is lowest than this **\n")
+    elif num1<num :
+        print ("\n** number is biger than this **\n")
+    elif num1==num :
+        print ("\n\n **perfect ** \n\n")
+        break
